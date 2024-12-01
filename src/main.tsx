402707f85f98c40de3d9466811be9d0989957cab
Luke <luchenqun@qq.com>
2024-12-01 20:44:34 +0800

    login always success

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,8 @@ import checkLogin from './utils/checkLogin';
 import changeTheme from './utils/changeTheme';
 import useStorage from './utils/useStorage';
 import './mock';
+import Mock from 'mockjs';
+import { generatePermission } from '@/routes';
 
 const store = createStore(rootReducer);
 
@@ -39,12 +41,34 @@ function Index() {
       type: 'update-userInfo',
       payload: { userLoading: true },
     });
-    axios.get('/api/user/userInfo').then((res) => {
-      store.dispatch({
-        type: 'update-userInfo',
-        payload: { userInfo: res.data, userLoading: false },
-      });
+    // axios.get('/api/user/userInfo').then((res) => {
+    const userRole = window.localStorage.getItem('userRole') || 'admin';
+    store.dispatch({
+      type: 'update-userInfo',
+      payload: {
+        userInfo: {
+          name: 'admin',
+          avatar:
+            'https://lf1-xgcdn-tos.pstatp.com/obj/vcloud/vadmin/start.8e0e4855ee346a46ccff8ff3e24db27b.png',
+          email: 'wangliqun@email.com',
+          job: 'frontend',
+          jobName: '前端开发工程师',
+          organization: 'Frontend',
+          organizationName: '前端',
+          location: 'beijing',
+          locationName: '北京',
+          introduction: '王力群并非是一个真实存在的人。',
+          personalWebsite: 'https://www.arco.design',
+          verified: true,
+          phoneNumber: /177[*]{6}[0-9]{2}/,
+          accountId: /[a-z]{4}[-][0-9]{8}/,
+          registrationTime: Mock.Random.datetime('yyyy-MM-dd HH:mm:ss'),
+          permissions: generatePermission(userRole),
+        },
+        userLoading: false,
+      },
     });
+    // });
   }
 
   useEffect(() => {
