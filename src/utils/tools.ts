c2a7ecd3b9673f4adf1fe69acf20ee096cdd3708
Luke <luchenqun@qq.com>
2024-12-17 22:37:21 +0800

    feat: fix bug

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,26 +1,13 @@
 import { verifyMessage } from 'viem';
-
+import BigNumber from 'bignumber.js';
 export const SevenDaySeconds = 7 * 24 * 3600;
 export const UsdtPrecision = 1000000n;
 export const MudPrecision = 1000000000000000000n;
 
-export const humanReadable = (value: number | bigint | string, precision = 1000000000000000000n) => {
-  if (value === undefined || value === null) return '-';
-  value = BigInt(value);
-  if (value === 0n) return '0';
-
-  const result = ((value * 100n) / precision).toString(); // 将结果扩大100倍以保留两位小数
-  const integerPart = result.slice(0, -2) || '0'; // 获取整数部分
-  const decimalPart = result.slice(-2); // 获取小数部分
-
-  // 如果小数部分以0结尾,则去掉末尾的0
-  if (decimalPart === '00' || decimalPart === '0') {
-    return integerPart;
-  } else if (decimalPart.endsWith('0')) {
-    return `${integerPart}.${decimalPart[0]}`;
-  } else {
-    return `${integerPart}.${decimalPart}`;
-  }
+export const humanReadable = (value: number | bigint | string, precision = 1000000000000000000n, property = BigNumber.ROUND_DOWN) => {
+  const b = new BigNumber(value);
+  const result = b.dividedBy(precision).toFixed(2, property);
+  return result;
 };
 
 // mudToUsdt
